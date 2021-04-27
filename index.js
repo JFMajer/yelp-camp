cd2337b8de2f88d425b49c49bd798ca27e6fbcf2
JFMajer <jakubmajer@live.com>
2021-04-27 19:49:14 +0200

    added ejs-mate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 
 //database connection
